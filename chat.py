d1acabc983d3c5ef668eb553183b59a205479af1
Fish <fishhuang068@gmail.com>
2021-04-29 01:06:42 +0800

    add LINE chat

--- chat.py
+++ chat.py
@@ -77,3 +77,65 @@ def main():
     write_file('output.txt', lines)
     
 main()
+
+#-----------------
+# LINE 的對話紀錄
+#-----------------
+    
+def read_file(filename):
+    lines = []
+    with open(filename, 'r', encoding = 'utf-8-sig') as f: #\ufeff 會出現奇怪空格, utf-8-sig可以解決此問題
+        for line in f:
+            lines.append(line.strip())
+    return lines
+
+
+def convert(lines):
+    new = []
+    person = None
+    allen_word_count = 0
+    viki_word_count = 0
+    allen_sticker_count = 0
+    viki_sticker_count = 0    
+    allen_image_count = 0
+    viki_image_count = 0 
+    for line in lines:
+        s = line.split(' ')
+        time = s[0]
+        name = s[1]
+        if name == 'Allen':   
+            if s[2] == '貼圖':
+                allen_sticker_count += 1
+            elif s[2] == '圖片':
+                allen_image_count += 1
+            else : 
+                for m in s[2:]:
+                    allen_word_count += len(m)
+        elif name == 'Viki':   
+            if s[2] == '貼圖':
+                viki_sticker_count += 1
+            elif s[2] == '圖片':
+                viki_image_count += 1
+            else : 
+                for m in s[2:]:
+                    viki_word_count += len(m)
+    print('Allen 說了', allen_word_count, "個字，傳了", allen_sticker_count, '個貼圖', allen_image_count, '個圖片')
+    print('Viki 說了', viki_word_count, "個字，傳了", viki_sticker_count, '個貼圖', viki_image_count, '個圖片')
+    return new
+    
+
+def write_file(filename, lines):
+    with open(filename, 'w') as f:
+        for line in lines:
+            f.write(line + '\n')
+
+    
+def main():
+    lines = read_file('LINE-Viki.txt')
+    lines = convert(lines)
+    #write_file('output.txt', lines)
+    
+main()
+
+# Allen 說了 96 個字，傳了 0 個貼圖 2 個圖片
+# Viki 說了 163 個字，傳了 1 個貼圖 4 個圖片
